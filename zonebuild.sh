09c483471c80853730aaef66fddfc59a60ebf406
BaptisS <56864846+BaptisS@users.noreply.github.com>
2023-04-18 19:26:11 +0200

    Update zonebuild.sh

--- zonebuild.sh
+++ zonebuild.sh
@@ -11,12 +11,15 @@ if [ -z "$zone_exist" ]
   zone=$(oci dns zone create --compartment-id $target_comp --name $zonename --zone-type "PRIMARY" --scope "PRIVATE" --view-id $target_viewid) 
   zoneid=$(echo $zone | jq -r '.data | ."id"')
   echo $zone | jq -r '.' > mirrored_$zoneid_$zonename.json
+  echo New zone $zonename created - $zoneid
 else 
 zoneid=$(cat $zone_exist | jq -r '.data | ."id"')
+echo zone $zonename already exist with ocid $zoneid
 fi
 
 cat $filename | jq -r '[.items[] | select (."rtype" | startswith("A","PTR"))]' > records_$filename.tmp
 sed -i 's/true/false/g' records_$filename.tmp
-oci dns record zone patch --zone-name-or-id $zoneid --scope "PRIVATE" --view-id $target_viewid --items file://records_$filename.tmp  
+patch=$(oci dns record zone patch --zone-name-or-id $zoneid --scope "PRIVATE" --view-id $target_viewid --items file://records_$filename.tmp)
+echo $patch | jq -r ' .data.items[] | [."domain", ."rdata"] | @csv'
 rm -f records_$filename.tmp
 
