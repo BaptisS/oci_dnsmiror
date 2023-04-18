0ebc3bb0d23f444ca24e8985c804845ab456acef
BaptisS <56864846+BaptisS@users.noreply.github.com>
2023-04-18 18:13:35 +0200

    Update zonebuild.sh

--- zonebuild.sh
+++ zonebuild.sh
@@ -5,14 +5,9 @@ export target_comp=$2
 export target_viewid=$3
 
 zonename=$(cat $filename | jq -r '. | ."domain"')
-zone=$(oci dns zone create --compartment-id $target_comp --name $zonename --zone-type "PRIMARY" --scope "PRIVATE" --view-id $target_viewid) 
-zoneid=$(echo $zone | jq -r '.data | ."id"')
-echo $zone | jq -r '.' > mirrored_$zoneid_$zonename.json
 
-#cat $filename | jq -r '[.items[] | select (."rtype" | startswith("A"))]' > arecords_$filename.tmp
-#sed -i 's/true/false/g' arecords_$filename.tmp
-#oci dns record zone patch --zone-name-or-id $zoneid --scope "PRIVATE" --view-id $target_viewid --items file://arecords_$filename.tmp
-#rm -f arecords_$filename.tmp 
+zone_exist=$(grep $zonename mirrored_* -lR)
+if [ -z "$zone_exist" ]; then zone=$(oci dns zone create --compartment-id $target_comp --name $zonename --zone-type "PRIMARY" --scope "PRIVATE" --view-id $target_viewid) && zoneid=$(echo $zone | jq -r '.data | ."id"') ; else zoneid=$(cat $zone_exist | jq -r '.data | ."id"') && echo $zone | jq -r '.' > mirrored_$zoneid_$zonename.json; fi
 
 cat $filename | jq -r '[.items[] | select (."rtype" | startswith("A","PTR"))]' > records_$filename.tmp
 sed -i 's/true/false/g' records_$filename.tmp
