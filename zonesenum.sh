2352b9ad8888660987dcedd451ab50d32fe8a2dd
BaptisS <56864846+BaptisS@users.noreply.github.com>
2023-04-18 10:48:52 +0200

    Update zonesenum.sh

--- zonesenum.sh
+++ zonesenum.sh
@@ -12,15 +12,15 @@ sed -i '1,3d' zoneenum_$1.json
 head -n -5 zoneenum_$1.json > zoneenum_fixed_$1.json
 
 echo "{" > zoneexport_$1.json
-echo ""'"compartmentId"'": "'"'$zone_compid"'"'," >> zoneexport_$1.json
-echo ""'"domain"'": "'"'$zone_dom"'"'," >> zoneexport_$1.json
+echo ""'"compartmentId"'": "'"'$zone_compid'"'"," >> zoneexport_$1.json
+echo ""'"domain"'": "'"'$zone_dom'"'"," >> zoneexport_$1.json
 echo ""'"force"'": true," >> zoneexport_$1.json
 echo ""'"items"'": [" >> zoneexport_$1.json
 cat zoneenum_fixed_$1.json >> zoneexport_$1.json
 echo "]," >> zoneexport_$1.json
 echo ""'"scope"'": "'"PRIVATE"'"," >> zoneexport_$1.json
-echo ""'"viewId"'": "'"'$zone_viewid"'"'," >> zoneexport_$1.json
-echo ""'"zoneNameOrId"'": "'"'$zoneid"'"' >> zoneexport_$1.json
+echo ""'"viewId"'": "'"'$zone_viewid'"'"," >> zoneexport_$1.json
+echo ""'"zoneNameOrId"'": "'"'$zoneid'"'" >> zoneexport_$1.json
 echo "}" >> zoneexport_$1.json
 
 rm -f zoneenum_$1.json
