559297ffd0e6b295aaa272e32832a9ce1b0b3cea
BaptisS <56864846+BaptisS@users.noreply.github.com>
2023-04-18 17:06:35 +0200

    Update zonebuild.sh

--- zonebuild.sh
+++ zonebuild.sh
@@ -3,10 +3,19 @@
 export filename=$1
 export target_comp=$2
 export target_viewid=$3
+
 zonename=$(cat $filename | jq -r '. | ."domain"')
-zone=$(oci dns zone create --compartment-id $target_comp --name $zonename --zone-type "PRIMARY" --scope "PRIVATE" --view-id $target_viewid)
+zone=$(oci dns zone create --compartment-id $target_comp --name $zonename --zone-type "PRIMARY" --scope "PRIVATE" --view-id $target_viewid) 
 zoneid=$(echo $zone | jq -r '.data | ."id"')
-cat $filename | jq -r '[.items[] | select (."rtype" | startswith("A"))]' > arecords_$filename.tmp
-sed -i 's/true/false/g' arecords_$filename.tmp
-oci dns record zone patch --zone-name-or-id $zoneid --scope "PRIVATE" --view-id $target_viewid --items file://arecords_$filename.tmp
-rm -f arecords_$filename.tmp 
+echo $zone | jq -r '.' > mirrored_$zoneid_$zonename.json
+
+#cat $filename | jq -r '[.items[] | select (."rtype" | startswith("A"))]' > arecords_$filename.tmp
+#sed -i 's/true/false/g' arecords_$filename.tmp
+#oci dns record zone patch --zone-name-or-id $zoneid --scope "PRIVATE" --view-id $target_viewid --items file://arecords_$filename.tmp
+#rm -f arecords_$filename.tmp 
+
+cat $filename | jq -r '[.items[] | select (."rtype" | startswith("A","PTR"))]' > records_$filename.tmp
+sed -i 's/true/false/g' records_$filename.tmp
+oci dns record zone patch --zone-name-or-id $zoneid --scope "PRIVATE" --view-id $target_viewid --items file://records_$filename.tmp  
+rm -f records_$filename.tmp
+
