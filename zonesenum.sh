8dabdb3235a36b7160cf28e98d55b8787f3d4f17
BaptisS <56864846+BaptisS@users.noreply.github.com>
2023-04-18 09:02:38 +0200

    Create zonesenum.sh

---
+++ zonesenum.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#zonesenum.sh
+export zoneid=$1
+zonedetail=$(oci dns zone get --zone-name-or-id $zoneid)
+zone_compid=$(echo $zonedetail | jq -r '.[] | ."compartment-id"')
+zone_dom=$(echo $zonedetail | jq -r '.[] | ."name"')
+zone_viewid=$(echo $zonedetail | jq -r '.[] | ."view-id"')
+echo "Enumerating Domain : '"$zone_dom"'"
+
+oci dns record zone get --zone-name-or-id $zoneid --all > zoneenum_$1.json
+sed -i '1,3d' zoneenum_$1.json
+head -n -5 zoneenum_$1.json > zoneenum_fixed_$1.json
+
+echo "{" > zoneexport_$1.json
+echo "  "'"compartmentId"'": '"$zone_compid"'," >> zoneexport_$1.json
+echo "  "'"domain"'": '"$zone_dom"'," >> zoneexport_$1.json
+echo "  "'"force"'": true," >> zoneexport_$1.json
+echo "  "'"items"'": [" >> zoneexport_$1.json
+cat zoneenum_fixed_$1.json >> zoneexport_$1.json
+echo "  ]," >> zoneexport_$1.json
+echo "  "'"scope"'": "PRIVATE"," >> zoneexport_$1.json
+echo "  "'"viewId"'": '"$zone_viewid"'," >> zoneexport_$1.json
+echo "  "'"zoneNameOrId"'": '"$zoneid"'" >> zoneexport_$1.json
+echo "}" >> zoneexport_$1.json
