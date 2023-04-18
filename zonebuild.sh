bc9b0a6fb55815ea3548c5b8cb5f71c079a5d0c1
BaptisS <56864846+BaptisS@users.noreply.github.com>
2023-04-18 15:57:52 +0200

    Update zonebuild.sh

--- zonebuild.sh
+++ zonebuild.sh
@@ -8,4 +8,5 @@ zone=$(oci dns zone create --compartment-id $target_comp --name $zonename --zone
 zoneid=$(echo $zone | jq -r '.data | ."id"')
 cat $filename | jq -r '[.items[] | select (."rtype" | startswith("A"))]' > arecords_$filename.tmp
 sed -i 's/true/false/g' arecords_$filename.tmp
-oci dns record zone patch --zone-name-or-id $zoneid --scope "PRIVATE" --view-id $target_viewid --items file://arecords_$filename.tmp  
+oci dns record zone patch --zone-name-or-id $zoneid --scope "PRIVATE" --view-id $target_viewid --items file://arecords_$filename.tmp
+rm -f arecords_$filename.tmp 
